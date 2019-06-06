856be1c4c04dc0fef6e8b977e85c80a50343112a
kalaikannan26s <51432038+kalaikannan26s@users.noreply.github.com>
2019-06-06 11:27:59 +0530

    Create odd1.py

---
+++ odd1.py
@@ -0,0 +1,9 @@
+b=int(input("enter the number"))
+if b>0:
+        if b%2==0:
+              print("this is an even number")
+        elif b%2!=0:
+                print("this is an odd  number")
+else:
+        print("this is an invalid number")
+
